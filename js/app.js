ead2d2ab22fde0bfd5ef697f5db3efc549a0d010
Juan Jose Mendoza Becerril <juanjo_mendoza130@outlook.com>
2024-05-06 17:19:13 -0600

    first-commit

---
+++ js/app.js
@@ -0,0 +1,97 @@
+'use-strict'
+
+const toggleBtn = document.querySelector('.toggle-btn')
+        const toggleBtnIcon = document.querySelector('.toggle-btn i')
+        const dropdownMenu = document.querySelector('.dropdown_menu')
+
+        toggleBtn.onclick = function(){
+            dropdownMenu.classList.toggle('open')
+            const isOpen = dropdownMenu.classList.contains('open')
+
+            toggleBtnIcon.classList = isOpen
+                ? 'fa-solid fa-xmark'
+                : 'fa-solid fa-bars'
+        }
+
+        const items = document.querySelectorAll('.slider .list .item');
+        const next = document.querySelector('.arrows .next');
+        const prev = document.querySelector('.arrows .prev');
+
+        // config param
+        let countItem = items.length;
+        let itemActive = 0;
+        // event next click
+        next.onclick = function(){
+            itemActive = itemActive + 1;
+            if(itemActive >= countItem){
+                itemActive = 0;
+            }
+            showSlider();
+        }
+        //event prev click
+        prev.onclick = function(){
+            itemActive = itemActive - 1;
+            if(itemActive < 0){
+                itemActive = countItem - 1;
+            }
+            showSlider();
+            }
+            // auto run slider
+            let refreshInterval = setInterval(() => {
+                next.click();
+            }, 5000)
+            function showSlider(){
+            // remove item active old
+                let itemActiveOld = document.querySelector('.slider .list .item.active');
+                itemActiveOld.classList.remove('active');
+                
+
+                // active new item
+                items[itemActive].classList.add('active');
+        
+
+                // clear auto time run slider
+                clearInterval(refreshInterval);
+                refreshInterval = setInterval(() => {
+                    next.click();
+                }, 5000)
+            }
+        
+function scrollToSection(sectionId){
+    const section = document.getElementById(sectionId);
+    if(section){
+        section.scrollIntoView({behavior: 'smooth'});
+    }
+}
+
+let currentLanguage = 'en'; // Establece el idioma predeterminado
+
+function changeLanguage(language) {
+    currentLanguage = language;
+    updateContent();
+}
+
+function updateContent() {
+    fetch(`${currentLanguage}.json`)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelectorAll('btn-home').textContent = data.btn_home;
+        document.getElementById('btn-about').textContent = data.btn_about;
+        document.getElementById('btn-services').textContent = data.btn_services;
+        document.getElementById('btn-contact').textContent = data.btn_contact;
+        document.getElementById('slogan').textContent = data.slogan;
+        document.getElementById('quienes-somos-p').textContent = data.quienes_somos_p;
+        // Actualiza más textos aquí según sea necesario
+    })
+    .catch(error => console.error('Error loading language file:', error));
+}
+
+// Carga el contenido inicial en el idioma predeterminado
+updateContent();
+
+
+
+
+
+         
+            
