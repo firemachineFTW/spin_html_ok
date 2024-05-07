eb2a82791754135b32860e9292defeef8c4bcbbc
Juan Jose Mendoza Becerril <juanjo_mendoza130@outlook.com>
2024-05-07 17:52:06 -0600

    second-commit-2

--- js/contact.js
+++ js/contact.js
@@ -10,3 +10,25 @@ const toggleBtn = document.querySelector('.toggle-btn')
                 ? 'fa-solid fa-xmark'
                 : 'fa-solid fa-bars'
         }
+
+let currentLanguage = 'en';
+
+function changeLanguage(language){
+    currentLanguage = language;
+    updateContent();
+}
+
+function updateContent(){
+    fetch(`${currentLanguage}.json`)
+    .then(response => response.json())
+    .then(data =>{
+        document.getElementById('title-contact').textContent = data.btn_contact;
+        document.getElementById('link-home').textContent = data.btn_home;
+        document.getElementById('link-about').textContent = data.btn_about;
+        document.getElementById('link-services').textContent = data.btn_services;
+        document.getElementById('link-contact').textContent = data.btn_contact;
+    })
+    .catch(error => console.error("Error loading language file", error))
+}
+
+updateContent();
